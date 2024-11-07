69f12511e391e0cfe02067ce4b70bd94a5ff861a
BaroczekRK <104620415+Karol-Rejek@users.noreply.github.com>
2024-11-07 07:17:44 +0100

    school

---
+++ MauiINF04/MauiApp0624/Elements/Objects/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp0624.Elements.Objects
+{
+    internal class Data
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string SongsNumber { get; set; }
+        public int Year { get; set; }
+        public int DownloadNumber { get; set; }
+
+        private Data(string artist, string album, string songsNumber, int year, int downloadNumber)
+        {
+            this.Artist = artist;
+            this.Album = album;
+            this.SongsNumber = songsNumber;
+            this.Year = year;
+            this.DownloadNumber = downloadNumber;
+        }
+    }
+}
---
+++ MauiINF04/MauiApp0624/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiApp0624.Elements.Objects;
+
+namespace MauiApp0624
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<Data> datas;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.datas = new List<Data>();
+
+        }
+
+        //private Data ReadFromFile()
+        //{
+
+        //    return Data();
+        //}
+
+        private void Button_Clicked_Back(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_Next(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}

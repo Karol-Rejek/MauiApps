7c17ba85b5d90e7f5d6b8882adf1355d9ff17e3e
BaroczekRK <104620415+Karol-Rejek@users.noreply.github.com>
2025-01-21 19:09:57 +0100

    FirstApp

---
+++ MauiINF04/MauiApp0624/Elements/Objects/Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp0624.Elements.Objects
+{
+    internal class Album
+    {
+        public string Artist { get; set; }
+        public string AlbumName { get; set; }
+        public int SongsNumber { get; set; }
+        public int Year { get; set; }
+        public int DownloadNumber { get; set; }
+
+        public Album() { }
+
+        public Album(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            this.Artist = artist;
+            this.AlbumName = album;
+            this.SongsNumber = songsNumber;
+            this.Year = year;
+            this.DownloadNumber = downloadNumber;
+        }
+    }
+}
---
+++ MauiINF04/MauiApp0624/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using MauiApp0624.Elements.Objects;
+
+namespace MauiApp0624
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<Album> albums;
+        public int currentAlbumIndex = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.albums = new List<Album>();
+
+            ReadFromFile("Data.txt");
+
+            //string filepath = Path.Combine(AppContext.BaseDirectory, "Data.txt");
+            //using (StreamReader reader = new(filepath)) 
+            //{
+            //    string line, newline;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        Data newData = new Data()
+            //        { 
+            //            Artist = line,
+            //            Album = reader.ReadLine(),
+            //            SongsNumber = int.Parse(reader.ReadLine()),
+            //            Year = int.Parse(reader.ReadLine()),
+            //            DownloadNumber = int.Parse(reader.ReadLine()),
+            //        };
+            //        newline = reader.ReadLine();
+            //        datas.Add(newData);
+            //    }
+            //}
+            UpdateCurrentData();
+        }
+
+        private void ReadFromFile(string file) // READ FROM FILE AND SAVE DATA IN COLECTION
+        {
+            string filePath = Path.Combine(AppContext.BaseDirectory, file);
+            using (StreamReader reader = new(filePath))
+            {
+                string lineArtist;
+                while ((lineArtist = reader.ReadLine()) != null)
+                {
+                    Album newData = new Album()
+                    {
+                        Artist = lineArtist,
+                        AlbumName = reader.ReadLine(),
+                        SongsNumber = int.Parse(reader.ReadLine()),
+                        Year = int.Parse(reader.ReadLine()),
+                        DownloadNumber = int.Parse(reader.ReadLine()),
+                    };
+                    reader.ReadLine();
+                    albums.Add(newData);
+                }
+            }
+        }
+
+        private void UpdateCurrentData() // UPDATE CURRENT SHOWED DATA ELEMENT
+        {
+            artistLabel.Text = albums[currentAlbumIndex].Artist;
+            albumLabel.Text = albums[currentAlbumIndex].AlbumName;
+            songsNumberLabel.Text = albums[currentAlbumIndex].SongsNumber.ToString() + " utworów";
+            yearLabel.Text = albums[currentAlbumIndex].Year.ToString();
+            downloadsLabel.Text = albums[currentAlbumIndex].DownloadNumber.ToString();
+        }
+
+        private void Button_Clicked_Back(object sender, EventArgs e) // GO TO PREVIOUS ELEMENT
+        {
+            currentAlbumIndex = (currentAlbumIndex + 1) % albums.Count;
+            UpdateCurrentData();
+        }
+
+        private void Button_Clicked_Next(object sender, EventArgs e) // GO TO NEXT ELEMENT
+        {
+            currentAlbumIndex = (currentAlbumIndex - 1 + albums.Count) % albums.Count;
+            UpdateCurrentData();
+        }
+
+        private void Button_Clicked_Download(object sender, EventArgs e) // INCREASED DOWNLOADS NUMBER
+        {
+            albums[currentAlbumIndex].DownloadNumber++;
+            UpdateCurrentData();
+        }
+    }
+
+}
